66d8cf12b4d920bd9c495f71d214e504e3ca397f
Sporny <janheidolph@yahoo.de>
2022-01-19 08:23:09 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,7 +8,10 @@ version '1.0.0'
 ui_page 'html/index.html'
 
 client_script 'client/main.lua'
-server_script 'server/main.lua'
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
 
 files {
     'html/index.html',
